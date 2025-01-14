d2d5cd145b35d205ee4600256f50314cfb3a210b
bvieuble <bastien.vieuble@gmail.com>
2025-01-14 19:24:01 +0800

    CAS-ANLA map

--- assets/js/leaflet-setup.js
+++ assets/js/leaflet-setup.js
@@ -15,9 +15,15 @@ document.addEventListener("readystatechange", () => {
       //   maxZoom: 19,
       //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       // }).addTo(map);
-       L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//        L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
+// 	maxZoom: 18,
+// 	attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+       L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
       let geoJSON = L.geoJSON(JSON.parse(jsonData)).addTo(map);
       map.fitBounds(geoJSON.getBounds());
